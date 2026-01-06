ca2ff67d21fa2ede17dd3b94c399b1e3dce76731
David Nighswander <david.nighswander@gmail.com>
2026-01-05 22:46:18 -0600

    Updated to include gurathnaka hostname/config.

--- flake.nix
+++ flake.nix
@@ -16,6 +16,33 @@
     ...
     }: {
     nixosConfigurations = {
+      gurathnaka = nixpkgs.lib.nixosSystem {
+        modules = [
+	  ./hosts/gurathnaka.nix
+	  #./modules
+
+	  {nixpkgs.hostPlatform = "x86_64-linux";}
+	  {nixpkgs.config.allowUnfree = true;}
+
+	  ({ pkgs, ... }: {
+	    nix = {
+	      registry = {
+               nixpkgs.flake = nixpkgs;
+	      };
+	    };
+	  })
+
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.djnighs = import ./home-manager/default.nix;
+	    home-manager.extraSpecialArgs = {inherit inputs;};
+	    home-manager.backupFileExtension  = "hm-backup";
+	  }
+	];
+	specialArgs = {inherit inputs;};
+      };
       slaanesh = nixpkgs.lib.nixosSystem {
         modules = [
 	  ./hosts/slaanesh.nix
