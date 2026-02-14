5ec3d8f72b4cb1f2d1269508d425f2d6cad6b637
David Nighswander <david.nighswander@gmail.com>
2026-02-14 17:21:25 -0600

    merge and cleanup 6?/?

--- home-manager/hyprland-animportantfish.nix
+++ home-manager/hyprland-animportantfish.nix
@@ -148,10 +148,10 @@ in {
       bind = $mod, 33, pseudo, # P
       bind = $mod, 44, togglesplit, # J
       
-      bind = $mod, 44, movefocus, l # L
-      bind = $mod, 43, movefocus, r # H
+      bind = $mod, 43, movefocus, l # L
+      bind = $mod, 46, movefocus, r # H
       bind = $mod, 45, movefocus, u # K
-      bind = $mod, 46, movefocus, d # J
+      bind = $mod, 44, movefocus, d # J
       
       #bind = $mod, 113, movefocus, l # left arrow
       #bind = $mod, 114, movefocus, r # right arrow
